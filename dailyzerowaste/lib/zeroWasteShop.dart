fca1dad77fb3b82b2117be0b2fe25794811b7fd9
ningpop <craft1933@naver.com>
2021-03-22 02:57:35 +0900

    [ningpop] : add shop list page, shop marker with info mini page

--- dailyzerowaste/lib/zeroWasteShop.dart
+++ dailyzerowaste/lib/zeroWasteShop.dart
@@ -1,9 +1,16 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:dailyzerowaste/model/user.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'zeroWasteShopList.dart';
+
+const LatLng DEST_LOCATION = LatLng(37.553609, 126.911598);
+const double PIN_VISIBLE_POSITION = 257;
+const double PIN_INVISIBLE_POSITION = -220;
 
 class ZeroWasteShop extends StatefulWidget {
   ZeroWasteShop(User currentUser);
@@ -18,15 +25,32 @@ class _shop extends State<ZeroWasteShop> {
   Completer<GoogleMapController> _mapController = Completer();
   MapType _googleMapType = MapType.normal;
 
+  Set<Marker> _markers = Set<Marker>();
+
+  LatLng shopLocation;
+  BitmapDescriptor shopIcon;
+
+  double pinPillPosition = PIN_INVISIBLE_POSITION;
+
   CameraPosition _initialCameraPosition = CameraPosition(
     target: LatLng(36.769691, 126.931705),
-    zoom: 14,
+    zoom: 17,
   );
 
   void _onMapCreated(GoogleMapController controller) {
     if (!_mapController.isCompleted) {
       _mapController.complete(controller);
     }
+    showPinsOnMap();
+  }
+
+  void setShopMarkerIcons() async {
+    shopIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(devicePixelRatio: 2.0), 'assets/icon/location.png');
+  }
+
+  void setInitialLocation() {
+    shopLocation = LatLng(DEST_LOCATION.latitude, DEST_LOCATION.longitude);
   }
 
   // 현재 위치 불러오기
@@ -49,6 +73,28 @@ class _shop extends State<ZeroWasteShop> {
     ));
   }
 
+  void showPinsOnMap() {
+    setState(() {
+      _markers.add(Marker(
+        markerId: MarkerId('abc'),
+        position: shopLocation,
+        icon: shopIcon,
+        onTap: () {
+          setState(() {
+            this.pinPillPosition = PIN_VISIBLE_POSITION;
+          });
+        },
+      ));
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.setInitialLocation();
+    this.setShopMarkerIcons();
+  }
+
   @override
   Widget build(BuildContext context) {
     // 배경 이미지
@@ -65,24 +111,158 @@ class _shop extends State<ZeroWasteShop> {
             Container(
               padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
               child: GoogleMap(
+                markers: _markers,
                 mapType: _googleMapType,
                 onMapCreated: _onMapCreated,
                 initialCameraPosition: _initialCameraPosition,
                 myLocationEnabled: true,
+                onTap: (LatLng loc) {
+                  // 마커를 눌렀을 때 가게 정보 띄우기
+                  setState(() {
+                    this.pinPillPosition = PIN_INVISIBLE_POSITION;
+                  });
+                },
               ),
             ),
-            Container(
-              padding: EdgeInsets.only(top: 90, right: 10),
-              alignment: Alignment.topRight,
-              child: FloatingActionButton(
-                child: Icon(Icons.menu),
-                backgroundColor: Color(0xffede6dd),
-                foregroundColor: Color(0xff4e4b49),
-                onPressed: _currentLocation,
-              ),
+            ShopListButton(),
+            AnimatedPositioned(
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeInOut,
+              left: 0,
+              right: 0,
+              bottom: this.pinPillPosition,
+              child: ShopInfo(),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ShopInfo extends StatelessWidget {
+  const ShopInfo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(0),
+      padding: EdgeInsets.all(0),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("image/shop_info.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'AlmangMarket',
+                  style: TextStyle(
+                    fontFamily: 'Quick-Pencil',
+                    fontSize: 25,
+                  ),
+                ),
+                Text(
+                  'Seoul, Mapo-gu, Hapjeong-dong, Woldeukeom-ro, 49 한\n우마을 2층',
+                  style: TextStyle(
+                    fontFamily: 'Quick-Pencil',
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 23.5),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(bottom: 16),
+                  child: Row(
+                    children: <Widget>[
+                      SizedBox(width: 49.3),
+                      Icon(Icons.phone),
+                      SizedBox(width: 20.3),
+                      Text(
+                        '010-2229-1027',
+                        style: TextStyle(
+                          fontFamily: 'Quick-Pencil',
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(bottom: 16),
+                  child: Row(
+                    children: <Widget>[
+                      SizedBox(width: 49.3),
+                      Icon(Icons.access_time),
+                      SizedBox(width: 20.3),
+                      Text(
+                        '매일 14:00 ~ 20:00 (월요일 휴무)',
+                        style: TextStyle(
+                          fontFamily: 'Quick-Pencil',
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(bottom: 16),
+                  child: Row(
+                    children: <Widget>[
+                      SizedBox(width: 49.3),
+                      Icon(Icons.home_outlined),
+                      SizedBox(width: 20.3),
+                      Text(
+                        'http://www.instagram.com/almongmarket',
+                        style: TextStyle(
+                          fontFamily: 'Quick-Pencil',
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ShopListButton extends StatelessWidget {
+  const ShopListButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 98.5, right: 13.5),
+      alignment: Alignment.topRight,
+      child: FloatingActionButton(
+        child: Icon(Icons.menu),
+        backgroundColor: Color(0xffede6dd),
+        foregroundColor: Color(0xff4e4b49),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ZeroWasteShopList()),
+          );
+        },
       ),
     );
   }
