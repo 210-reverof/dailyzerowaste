c6a07d0143cf925139775166e8b6607db481de26
210_reverof <sheltonwon@gamil.com>
2021-03-22 01:57:12 +0900

    [210] : add viewFeed file (UI only)

---
+++ dailyzerowaste/lib/viewFeed.dart
@@ -0,0 +1,143 @@
+import 'package:dailyzerowaste/model/user.dart';
+import 'package:flutter/material.dart';
+
+class ViewFeedPage extends StatefulWidget {
+  ViewFeedPage(User currentUser);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _viewFeed();
+  }
+}
+
+class _viewFeed extends State<ViewFeedPage> {
+  List selectedTagBoxes = ["tag", "second", "third"];
+
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("image/background.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        body: Container(
+          child: ListView(
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // header
+                  SizedBox(height: 64),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(width: 36),
+                      Container(
+                        width: 26,
+                        child: InkWell(
+                          child: Image.asset('image/source_direction.png'),
+                          onTap: () => Navigator.pop(context),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 25),
+
+                  //태그 박스들
+                  Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                    SizedBox(width: 52.82),
+                    Wrap(
+                        spacing: 10.0,
+                        runSpacing: 20.0,
+                        children: selectedTagBoxes
+                            .map((option) => new Container(
+                                // margin: EdgeInsets.all(5),
+                                decoration: customBoxDecoration(),
+                                child: Container(
+                                    height: 20,
+                                    padding: EdgeInsets.all(5),
+                                    child: Text('$option',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            fontFamily: '1HoonDdukbokki',
+                                            fontSize: 11,
+                                            fontWeight: FontWeight.normal,
+                                            color: Colors.white)))))
+                            .toList()),
+                  ]),
+
+                  SizedBox(height: 21),
+
+                  //사진
+                  Container(
+                    height: 311,
+                    width: 311,
+                    color: Color(0xff4f4b49),
+                  ),
+
+                  SizedBox(height: 21),
+
+                  //제목
+                  Container(
+                      width: 311,
+                      child: Text("dddddddddddddddd",
+                          overflow: TextOverflow.visible,
+                          style: TextStyle(
+                              fontFamily: 'Quick-Pencil',
+                              fontSize: 30,
+                              fontWeight: FontWeight.normal,
+                              color: Color(0xff403e3d)))),
+
+                              SizedBox(height: 10),
+
+                  // 본문 내용
+                  Container(
+                      width: 311,
+                      child: Text("dddddddddddddddd",
+                          overflow: TextOverflow.visible,
+                          style: TextStyle(
+                              fontFamily: 'Quick-Pencil',
+                              fontSize: 20,
+                              fontWeight: FontWeight.normal,
+                              color: Color(0xff403e3d)))),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  customBoxDecoration() {
+    return BoxDecoration(
+      image: DecorationImage(
+          image: AssetImage("image/select_background.png"), fit: BoxFit.fill),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.5),
+          spreadRadius: 0.1,
+          blurRadius: 0.5,
+          offset: Offset(3, 3),
+        ),
+      ],
+      border: Border(
+          left: BorderSide(color: Colors.black12, width: 1.0),
+          bottom: BorderSide(color: Colors.black12, width: 1.0),
+          top: BorderSide(color: Colors.black12, width: 1.0),
+          right: BorderSide(color: Colors.black12, width: 1.0)),
+      borderRadius: const BorderRadius.all(
+        Radius.circular(5.0),
+      ),
+    );
+  }
+
+  changeState(item) {
+    setState(() {
+      item['isActive'] = !item['isActive'];
+    });
+  }
+}
